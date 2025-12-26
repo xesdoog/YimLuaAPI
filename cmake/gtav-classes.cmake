a2aaecda3725aa546d5d8144b3df309ba353169e
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-12-27 04:23:48 +0500

    chore: Update GTAV-Classes.

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Mr-X-GTA/GTAV-Classes-1.git
-    GIT_TAG        e803b20ec3d6cf20d6b254c915e088bd010f006e
+    GIT_TAG        b74e688860b0df47d7ec87c1eb6227ee853fc90a
     GIT_PROGRESS TRUE
     SOURCE_SUBDIR "why" # FetchContent_Populate is depricated now, so we set this to a non existing directory.
 )
